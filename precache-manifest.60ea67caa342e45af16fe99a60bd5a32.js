86af5d58c3930b547e77f945cda81cf6e616fb41
asa121asa <crafta.net@gmail.com>
2020-11-17 15:49:32 +0300

    Updates

---
+++ precache-manifest.60ea67caa342e45af16fe99a60bd5a32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a222ae209b14effc644fe6ccdc49fbc",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "38bda86316f871568989",
+    "url": "/Counter/static/css/main.63e2d10b.chunk.css"
+  },
+  {
+    "revision": "fd86f6e56a36f7ba68fb",
+    "url": "/Counter/static/js/2.b9b81f13.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Counter/static/js/2.b9b81f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38bda86316f871568989",
+    "url": "/Counter/static/js/main.be5d9b98.chunk.js"
+  },
+  {
+    "revision": "60d75ed92e1196d63d74",
+    "url": "/Counter/static/js/runtime-main.c2fba775.js"
+  }
+]);
